99878c438b4679dcfc9c0e8a15463fc062cf9d98
Dennis Bölling <derdennis1012@gmail.com>
2021-12-01 20:46:40 +0100

    Fixed Bugs

--- src/admin/pages/Settings.vue
+++ src/admin/pages/Settings.vue
@@ -3,15 +3,23 @@
     <h2>MQTT Plugin Pro Settings</h2>
     <hr />
     <div>
-      <b-form-group>
-        <label v-b-tooltip.hover.right title="z.B. 14.18.124.26 Ohne den Port"
-          >MQTT Broker URL</label
-        >
+      <b-form-group
+        v-b-tooltip.hover.right
+        title="z.B. 14.18.124.26 Ohne den Port"
+      >
+        <label>MQTT Broker URL</label>
         <b-form-input
           id="input-1"
           type="text"
           placeholder="Enter MQTT URL"
           v-model="settingsData.mqtt_url"
+          required
+        ></b-form-input>
+        <b-form-input
+          id="input-1"
+          type="text"
+          placeholder="Enter MQTT URL"
+          v-model="settingsData.mqtt_url2"
           required
         ></b-form-input>
       </b-form-group>
@@ -27,6 +35,8 @@
   @BoostrapDoc
   https://getbootstrap.com/docs/4.1/getting-started/introduction/
 
+  @Validation
+  https://vee-validate.logaretm.com/v4/
  -->
 <script>
 export default {
