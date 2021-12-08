6e813658b2a35452e231f120962cd2eee4adb60a
Josh <62060130+WhoKnows55@users.noreply.github.com>
2021-12-08 12:26:20 +0100

    Settings.vue added all fields

--- src/admin/pages/Settings.vue
+++ src/admin/pages/Settings.vue
@@ -3,12 +3,11 @@
     <h2>MQTT Plugin Pro Settings</h2>
     <hr />
 
-
+    <!--MQTT URL-->
     <div>
       <b-form-group
         v-b-tooltip.hover.right
-        title="z.B. 14.18.124.26 Ohne den Port"
-      >
+        title="z.B. 14.18.124.26 Ohne den Port">
         <label>MQTT Broker URL</label>
         <b-form-input
           id="url"
@@ -16,19 +15,17 @@
           placeholder="Enter MQTT URL without Port"
           v-model="settingsData.mqtt_url"
           required
-        ></b-form-input>
-
+        ></b-form-input> 
       </b-form-group>
     </div>
-
     <div>URL: {{ settingsData.mqtt_url }}</div>
-    <b-button @click="saveData" variant="primary">Speichern</b-button>
+    <b-button @click="saveData" variant="primary">Speichern</b-button> 
     
-
 
     <hr />
 
 
+    <!--MQTT Port-->
     <div>
       <b-form-group>
       <label>MQTT Broker Port</label>
@@ -41,10 +38,131 @@
         ></b-form-input>
       </b-form-group>
     </div>
-
     <div>Port: {{ settingsData.mqtt_port }}</div>
     <b-button @click="saveData" variant="primary">Speichern</b-button>
-    
+
+
+    <hr />
+
+
+    <!--MQTT client id-->
+    <div>
+      <b-form-group>
+      <label>MQTT Client ID</label>
+        <b-form-input
+          id="port"
+          type="text"
+          placeholder="Enter Client ID"
+          v-model="settingsData.mqtt_client_id"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </div>
+    <div>ID: {{ settingsData.mqtt_client_id }}</div>
+    <b-button @click="saveData" variant="primary">Speichern</b-button>
+
+    <hr />
+
+
+    <!--MQTT User: optional - Prüfen, ob User existiert-->
+    <div>
+      <b-form-group>
+      <label>MQTT User</label>
+        <b-form-input
+          id="port"
+          type="text"
+          placeholder="Enter User"
+          v-model="settingsData.mqtt_user"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </div>
+    <div>ID: {{ settingsData.mqtt_user }}</div>
+    <b-button @click="saveData" variant="primary">Speichern</b-button>
+
+
+    <hr />
+
+
+    <!--MQTT password: optional - Prüfen, ob pw korrekt-->
+    <div>
+      <b-form-group
+        v-b-tooltip.hover.right
+        title="Enter Password for User">
+      <label>MQTT Password</label>
+        <b-form-input
+          id="port"
+          type="password"
+          
+          v-model="settingsData.mqtt_password"
+          required
+        ></b-form-input>
+       
+      </b-form-group>
+    </div>
+     <!--div>ID: {{ settingsData.mqtt_password }}</div-->
+    <b-button @click="saveData" variant="primary">Speichern</b-button>
+
+
+    <hr />
+
+
+    <!--MQTT Topics-->
+    <div>
+      <b-form-group>
+      <label>MQTT Topics</label>
+        <b-form-input
+          id="port"
+          type="text"
+          placeholder="Comma-separated list of topics, like: sensor_1,sensor_2,..."
+          v-model="settingsData.mqtt_topics"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </div>
+    <div>ID: {{ settingsData.mqtt_topics }}</div>
+    <b-button @click="saveData" variant="primary">Speichern</b-button>
+
+
+    <hr />
+
+
+    <!--MQTT intervall-->
+    <div>
+      <b-form-group>
+      <label>MQTT Intervall</label>
+        <b-form-input
+          id="port"
+          type="number"
+          placeholder="Interval to wait for the MQTT values (in seconds)"
+          v-model="settingsData.mqtt_intervall"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </div>
+    <div>ID: {{ settingsData.mqtt_intervall }}</div>
+    <b-button @click="saveData" variant="primary">Speichern</b-button>
+
+
+    <hr />
+
+
+    <!--MQTT ttl-->
+    <div>
+      <b-form-group>
+      <label>MQTT Time To Live</label>
+        <b-form-input
+          id="port"
+          type="number"
+          placeholder="How long the MQTT values are stored (in days)"
+          v-model="settingsData.mqtt_ttl"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </div>
+    <div>ID: {{ settingsData.mqtt_ttl }}</div>
+    <b-button @click="saveData" variant="primary">Speichern</b-button>
+
 
   </div>
 </template>
