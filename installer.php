ae67a56cdc0aa8b18e8022cd2fc2423f5e3fe6f5
Dennis Bölling <derdennis1012@gmail.com>
2021-12-13 14:39:46 +0100

    Added Datatable

--- installer.php
+++ installer.php
@@ -13,6 +13,7 @@ if ( $wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name ) {
             PRIMARY KEY  (ID)
     ) $charset_collate;";
 
+
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
     add_option('my_db_version', $mqtt_pro_data_db_version);
--- src/admin/pages/Home.vue
+++ src/admin/pages/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
     <span>{{ msg }}</span>
+    <DataTable></DataTable>
   </div>
 </template>
 
 <script>
+import DataTable from '../components/DataTable.vue'
 export default {
 
   name: 'Home',
-
+  components:{
+    DataTable
+    },
   data () {
     return {
       msg: 'Welcome to Your Vue.js Admin App'
