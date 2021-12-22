768b591e19c268d47fb5a49b673bac3d14febd6c
Dennis Bölling <derdennis1012@gmail.com>
2021-12-22 11:57:13 +0100

    New Task for @lenascheit

--- src/admin/pages/SetupPageComponents/Step3.vue
+++ src/admin/pages/SetupPageComponents/Step3.vue
@@ -1,10 +1,17 @@
 <template>
-  <div
-    class="d-flex flex-column justify-content-between h-100"
-    v-if="step == 3"
-  >
+  <div class="d-flex flex-column justify-content-between h-100">
     <h1 class="mb-4 mt-4">3. Topics</h1>
-
+    <div>
+      <div
+        class="shadow rounded bg-white m-1"
+        v-for="(item, key) in data.topics"
+        :key="'topic-key-' + key"
+      >
+        {{ item }}
+      </div>
+    </div>
+    <b-button @click="data.topics.push('hello122')"> Add </b-button>
+    <div></div>
     <b-button variant="primary" block @click="checkNextStep"
       >Next Step</b-button
     >
