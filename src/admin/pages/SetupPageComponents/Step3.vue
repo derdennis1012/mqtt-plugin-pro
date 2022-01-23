a4f60731fe0cb1ab9f763647254607a1321df4e5
lenascheit <lena.scheit@hs-furtwangen.de>
2022-01-23 18:15:52 +0100

    Step 3 Regex

--- src/admin/pages/SetupPageComponents/Step3.vue
+++ src/admin/pages/SetupPageComponents/Step3.vue
@@ -9,46 +9,50 @@
         >3. Topics
       </h1>
       <div class="d-flex align-items-center justify-content-between"></div>
-      <div>
-        <h5>List of Topics:</h5>
-
-        <b-row>
-          <b-col sm="9">
-            <b-form-group>
-              <b-form-input
-                v-model="text"
-                placeholder="Add topic"
-                @keyup.enter="addItem"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col sm="3">
-            <div class="">
-              <b-button variant="success" block @click="addItem">
-                Add
-              </b-button>
-            </div>
-          </b-col>
-        </b-row>
+      <validation-observer ref="simpleRules">
         <div>
-          <ul class="list-group scroll-it">
-            <li
-              class="list-group-item my-2 border rounded"
-              v-for="(item, key) in data.topics"
-              :key="'topic-key-' + key"
-            >
-              <div class="d-flex align-items-center justify-content-between">
-                <b> {{ item }}</b>
-                <font-awesome-icon
-                  @click="deleteItem(key)"
-                  :icon="['fad', 'times']"
-                  class="text-danger"
-                />
+          <h5>List of Topics:</h5>
+          <b-row>
+            <b-col sm="9">
+              <b-form-group>
+                <validation-provider
+                  rules="required|regex:^\S*[^\/\s]$">
+                  <b-form-input
+                    v-model="text"
+                    placeholder="Add topic"
+                    @keyup.enter="addItem"
+                  />
+                </validation-provider>
+              </b-form-group>
+            </b-col>
+            <b-col sm="3">
+              <div class="">
+                <b-button variant="success" block @click="addItem">
+                  Add
+                </b-button>
               </div>
-            </li>
-          </ul>
+            </b-col>
+          </b-row>
+          <div>
+            <ul class="list-group scroll-it">
+              <li
+                class="list-group-item my-2 border rounded"
+                v-for="(item, key) in data.topics"
+                :key="'topic-key-' + key"
+              >
+                <div class="d-flex align-items-center justify-content-between">
+                  <b> {{ item }}</b>
+                  <font-awesome-icon
+                    @click="deleteItem(key)"
+                    :icon="['fad', 'times']"
+                    class="text-danger"
+                  />
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </validation-observer>
     </div>
     <b-button
       variant="primary"
@@ -60,9 +64,15 @@
   </div>
 </template>
 <script>
+
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+
 export default {
   name: "Step3",
-  components: {},
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
   props: {
     data: {
       type: Object,
